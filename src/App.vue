f7f559bb406178770d5bd6b92fd0ffbba4248123
yujinzheng <632463098@qq.com>
2021-08-09 01:20:40 +0800

    进度： 1、完成天猫置顶导航的开发

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <TopicIndex />
+</template>
+
+<script setup>
+import TopicIndex from './components/TopIndex.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.icon, .iconfont {
+  font-family:"iconfont" !important;
+  font-size:16px;
+  font-style:normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/TopIndex.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav class="top">
+        <a href="#nowhere">
+            <span class="icon iconfont icon-tmallhome2 redColor"></span>
+            天猫首页
+        </a>
+        <span>喵，欢迎来到天猫</span>
+        <a href="#nowhere">
+            请登录
+        </a>
+        <a href="#nowhere">
+            免费注册
+        </a>
+        <a href="#nowhere">
+            <span class="icon iconfont icon-tmallgouwuche2 redColor"></span>
+            购物车0件
+        </a>
+    </nav>
+</template>
+
+<style scoped>
+    a {
+        color: #999;
+        text-decoration: none;
+    }
+
+    nav.top {
+        font-size: 12px;
+        font-family: Arial;
+        background-color: #f2f2f2;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border-bottom: 1px solid #e7e7e7;
+    }
+
+    nav.top span, nav.top a {
+        color: #999;
+        margin: 0px 10px
+    }
+
+    .redColor {
+        color: #C40000 !important;
+    }
+
+    a:hover {
+        color: #C40000 !important;
+    }
+</style>
